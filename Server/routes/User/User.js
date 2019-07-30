41b79f846d3cf33d593547d03ae4c739155a8063
Farazjahangir <jahangirfaraz98@gmail.com>
2019-07-30 15:29:47 +0500

    Validation added

--- Server/routes/User/User.js
+++ Server/routes/User/User.js
@@ -8,28 +8,30 @@ const constants = require('../../config/constants')
 
 
 router.post('/register' , async (req, res)=>{
-    const userData = req.body
-    const userPassword = userData.password
+    
+    // const userData = req.body
+    // const userPassword = userData.password
+    const { userName , password } = req.body
+    // if(!userName) throw 'Username is Required'
+    if(!password) throw 'Password is Required'
 
     try{
         // Password Hashing
-        const hash = await bcrypt.hash(userPassword , 10)
-        userData.password = hash
+        const hash = await bcrypt.hash(password , 10)
+        req.body.password = hash
         // Saving user into database
-        let user = new User(userData)
+        let user = new User(req.body)
         const token = jwt.sign({id: user._id} , constants.SECRET_KEY)    
         user.token = token 
         await user.save()
         user = JSON.parse(JSON.stringify(user))
         delete user.password
-        res.send({
-            status: 200,
+        res.status(200).send({
             data: user,
-        })
+        })  
     }
     catch(e){
-        res.send({
-            status: 400,
+        res.status(400).send({
             errorMessage: e.message
         })
     }
@@ -38,36 +40,31 @@ router.post('/register' , async (req, res)=>{
 
 router.post('/login' , async (req,res)=>{
     const { userName, password } = req.body
+
+    if(!userName) throw 'Username is required'
+    if(!password) throw 'Password is required'
+
     try{
         // Finding user
         let user = await User.findOne({ userName })
         // If user not found
-        if(!user){
-            res.send({
-                status: 404,
-                errMessage: 'User does not exist'
-            })
-            return
-        }
+        if(!user) throw {message: 'User dose not exsit'}
+
         // password verification
         const isVerified = await bcrypt.compare(password, user.password);
 
         // If password not verified 
-        if(!isVerified){
-            res.send({
-                status: 404,
-                errMessage: 'Please check you username or password'
-            })
-        }
+        if(!isVerified) throw {message: 'Please check your username or password'}
+
         user = JSON.parse(JSON.stringify(user))
         delete user.password
-        res.send({
+        res.status(200).send({
             status: 200,
             data: user
         })
     }
     catch(e){
-        res.send({
+        res.status(404).send({
             status: 404,
             errMessage: e.message
         })
